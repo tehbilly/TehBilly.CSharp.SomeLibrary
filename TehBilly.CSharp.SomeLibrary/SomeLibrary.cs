a2b374c5086ab1bb5a76cd826b197f309bce57a3
William McGann <contact@williammcgann.com>
2023-11-14 14:34:58 -0500

    Initial commit

---
+++ TehBilly.CSharp.SomeLibrary/SomeLibrary.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+namespace TehBilly.CSharp.SomeLibrary;
+
+public interface ISomeLibrary
+{
+    public string Version();
+    public string FlipAString(string input);
+}
+
+public class SomeLibrary : ISomeLibrary
+{
+    public string Version() => System.Diagnostics.FileVersionInfo
+        .GetVersionInfo(Assembly.GetExecutingAssembly().Location)
+        .FileVersion!;
+
+    public string FlipAString(string input)
+    {
+        var chars = input.ToCharArray();
+        Array.Reverse(chars);
+        return new string(chars);
+    }
+}
