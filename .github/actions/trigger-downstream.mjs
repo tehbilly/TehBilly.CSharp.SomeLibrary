bb6af32dfa89d8da83991870cef41e6498b1d9ed
William McGann <contact@williammcgann.com>
2023-11-14 16:54:29 -0500

    Add more debug information

--- .github/actions/trigger-downstream.mjs
+++ .github/actions/trigger-downstream.mjs
@@ -2,8 +2,16 @@ import { Octokit } from "@octokit/action";
 
 const octokit = new Octokit();
 
+console.log("Debug process.env:", process.env);
+
 const repo = process.env.DOWNSTREAM_REPO;
-console.log(`Triggering downstream workflow for ${repo}`);
+console.log(`Triggering downstream workflow for ${repo}:`, {
+    event_type: "library_updated",
+    client_payload: {
+        library: process.env.GITHUB_REPOSITORY,
+        version: process.env.VERSION,
+    },
+});
 
 const response = await octokit.rest.repos.createDispatchEvent({
     owner: process.env.GITHUB_REPOSITORY_OWNER,
