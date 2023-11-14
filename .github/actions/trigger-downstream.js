06291f0315c7ac124c8f8bfbd8b787436555df9e
William McGann <contact@williammcgann.com>
2023-11-14 16:07:25 -0500

    Update octokit calls

--- .github/actions/trigger-downstream.js
+++ .github/actions/trigger-downstream.js
@@ -4,17 +4,14 @@ const octokit = new Octokit();
 
 const repo = process.env.DOWNSTREAM_REPO;
 console.log(`Triggering downstream workflow for ${repo}`);
-const response = octokit.request({
-    method: "POST",
-    url: "/repos/{owner}/{repo}/dispatches",
+
+const response = await octokit.rest.repos.createDispatchEvent({
     owner: process.env.GITHUB_REPOSITORY_OWNER,
     repo: repo,
-    data: {
-        event_type: "library_updated",
-        client_payload: {
-            library: process.env.GITHUB_REPOSITORY,
-            version: process.env.GITHUB_REF,
-        },
+    event_type: "library_updated",
+    client_payload: {
+        library: process.env.GITHUB_REPOSITORY,
+        version: process.env.GITHUB_REF,
     },
 });
 console.log("Response:", response);
